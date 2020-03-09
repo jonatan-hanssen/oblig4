05605025ab5eca79ed51555356733e5f42792ae9
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2020-03-09 11:37:22 +0100

    begynte

---
+++ Legesystem.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+public class Legesystem {
+	public static void main(String[] args) throws 
+	FileNotFoundException, UgyldigListeIndeks, UlovligUtskrift 
+	{
+		Scanner scanner = new Scanner(Sys.in
+	}
+}
