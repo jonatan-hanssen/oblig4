b51d7ca5449e1c8eac9ed340e24688d86aa122b5
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2020-03-10 13:51:13 +0100

    hey

--- DatabaseTest.java
+++ DatabaseTest.java
@@ -1,8 +1,10 @@
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+
 class DatabaseTest {
-	public static void main(String[] args) throws FileNotFoundException{
+	public static void main(String[] args) throws FileNotFoundException {
 		Database db = new Database();
 		db.lesFraFil("fil.txt");
-		db.printDatabase();
+		//db.printDatabase();
 	}
 }
