69ad60881771a1911fc3f5b6e97193329be960b3
mazunki <rolferen@gmail.com>
2020-03-22 13:05:44 +0100

    import exception

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 	public static void main(String[] args) {
 		Database db = new Database();
-		db.lesFraFil("storfil.txt");
+		try {
+			db.lesFraFil("storfil.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("Filen ble ikke funnet");
+		}
 		Startmeny hovedmeny = new Startmeny(db);
 	}
 }
