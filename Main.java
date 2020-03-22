f22808f993102e49fba9217bb2115aa997a42f6f
mazunki <rolferen@gmail.com>
2020-03-22 14:07:06 +0100

    removed forced read from file

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
-import java.io.FileNotFoundException;
-
 public class Main {
 	public static void main(String[] args) {
 		Database db = new Database();
-		try {
-			db.lesFraFil("storfil.txt");
-		} catch (FileNotFoundException e) {
-			System.out.println("Filen ble ikke funnet");
-		}
 		Startmeny hovedmeny = new Startmeny(db);
 	}
 }
