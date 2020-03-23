f1d4dc6856401acd58c40708fd36324950ea12bf
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2020-03-23 12:02:20 +0100

    hey

--- Database.java
+++ Database.java
@@ -14,7 +14,7 @@ import java.io.BufferedWriter;//klasse som lager buffer for aa redusere konverte
 import java.io.FileOutputStream;//klasse som tar outputStream og skriver til fil
 
 import java.util.HashMap;
-
+import java.uitl.TreeMap;
 
 
 class Database implements DatabaseInterface {
