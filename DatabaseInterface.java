41b7d05ce56e947c06c18f35728844d1cef332a6
mazunki <rolferen@gmail.com>
2020-03-23 11:13:48 +0100

    comments and reorder

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -3,27 +3,34 @@ import java.io.IOException;
 import java.util.HashMap;
 
 interface DatabaseInterface {
+	// filer
 	public void lesFraFil(String filnavn) throws FileNotFoundException;
 	public void skrivTilFil(String filnavn) throws IOException;
-	public void printAlt();
-	public void printAntallTypeResept(Class<?> cls);
-	public void printTypeMisbruk(HashMap<String, Integer> liste);
-	
+
+	// opprette	
 	public Resept lagResept(String legenavn, int legemiddelId, int pasientId, int reit, String type) throws TypeNotFoundException;
 	public Lege lagLege(String navn, int kontrollid);
 	public Pasient lagPasient(String navn, String fnr);
 	public Legemiddel lagLegemiddel(String navn, double pris, double virkestoff, int styrke, String type) throws TypeNotFoundException;
 
+	// vise
 	public void printPasient();
 	public void printResept();
 	public void printLege();
 	public void printLegemiddel();
+	public void printAlt();
 
+	// stats
+	public void printAntallTypeResept(Class<?> cls);
+	public void printTypeMisbruk(HashMap<String, Integer> liste);
+
+	// data
 	public SortertLenkeliste<Lege> hentLeger();
 	public Lenkeliste<Legemiddel> hentLegemidler();
 	public Lenkeliste<Resept> hentResepter();
 	public Lenkeliste<Pasient> hentPasienter();
 
+	// soeke
 	public Lege finnLege(String legenavn);
 	public Legemiddel finnLegemiddel(int id);
 	public Pasient finnPasient(int id);
